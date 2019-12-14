3025866b8a11aae8e9847faa47a79313328c80af
sebagruia <sebastiangruia@gmail.com>
2019-12-14 20:00:49 +0100

    Woking on conecting server to database

--- server.js
+++ server.js
@@ -1,23 +1,48 @@
 const express = require('express');
+const cors = require('cors');
+const bcrypt = require('bcryptjs');// A library used for "hashing" the password
+const knex = require('knex');//A library used to connect the server to a database
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'test123',
+        database: 'todolist'
+    }
+});
+
+
 
 const app = express();
 
 const dataBase = {
     users: [
         {
-            id: '1',
+            id: 1,
             name: 'Seba',
             email: 'blackthorus@gmail.com',
             password: '012',
+            container: [],
             joined: new Date()
         },
 
         {
-            id: '2',
+            id: 2,
             name: 'Ovi',
             email: 'ovidiu@gmail.com',
             password: '234',
+            container: [],
             joined: new Date()
+        }
+    ],
+
+    login: [
+        {
+            id: '908',
+            hash: '',
+            email: 'blackthorus@gmail.com'
         }
     ]
 }
@@ -26,37 +51,81 @@ const dataBase = {
 //===Middleware===
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(cors()); //Cors is for fixing security issues with Chrome
 // app.use(express.static(__dirname + '/public'));
 // ===============
 
 app.get('/', (req, res) => {
-    res.send('THIS IS WORKING');
+    res.json(dataBase.users);
 });
 
-app.post('/signin', (req, res) => {
-    if (req.body.email === dataBase.users[0].email && req.body.password === dataBase.users[0].password) {
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    //   // ===This function hashes the password===
+    //   bcrypt.genSalt(10, function(err, salt) {
+    //     bcrypt.hash(password, salt, function(err, hash) {
+    //     });
+    // });
+    // =========================================
+    if (email === dataBase.users[0].email && password === dataBase.users[0].password) {
         // res.send('Succes');  the send() can be used, but "express" comes with json() that has more functionalities
-        res.json('Succes');
+        res.json(dataBase.users[0]);
     }
-    else{
+    else {
         res.status(400).json('error logging in');
     }
 });
 
-app.post('/register', (req, res)=>{
-    const {name, email, password} = req.body;
-    dataBase.users.push(
-        {
-            id: '3',
+app.post('/register', (req, res) => {
+    const { name, email, password } = req.body;
+    // ===This function hashes the password===
+    // bcrypt.genSalt(10, function(err, salt) {
+    //     bcrypt.hash(password, salt, function(err, hash) {
+    //     });
+    // });
+    // =========================================
+    db('users')
+        .returning('*')//A knex function - is returning the "user" to be inserted
+        .insert({
             name: name,
             email: email,
-            password: password,
+            container: [],
             joined: new Date()
-        }
-    );
-    res.json(dataBase.users[dataBase.users.length-1]);
+        })
+        .then(user => res.json(user[0]))
+        .catch(err => res.status(400).json('Unable to Register. User allready exists'));
+
 
 });
+
+app.get('/profile/:id', (req, res) => {
+    const { id } = req.params;
+    db.select('*').from('users').where({ id: id })
+        .then(user => {
+            if (user.length) {
+                res.json(user[0]);
+            }
+            else {
+                res.json('User Not Found');
+            }
+        })
+        .catch(err => res.json('Error Getting User'));
+});
+
+
+
+// // Load hash from your password DB.
+// bcrypt.compare("B4c0/\/", hash, function(err, res) {
+//     // res === true
+// });
+// bcrypt.compare("not_bacon", hash, function(err, res) {
+//     // res === false
+// });
+
+// // As of bcryptjs 2.4.0, compare returns a promise if callback is omitted:
+// bcrypt.compare("B4c0/\/", hash).then((res) => {
+//     // res === true
+// });
 
 app.listen(4000);
 
